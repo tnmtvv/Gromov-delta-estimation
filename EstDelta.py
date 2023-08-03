6cdfe09b3d1a234755193d34bd52b4c0b0298d5e
tnmtvv <62231627+tnmtvv@users.noreply.github.com>
2023-08-03 21:14:31 +0300

    Update EstDelta.py

--- EstDelta.py
+++ EstDelta.py
@@ -33,20 +33,20 @@ def build_csv(
     datasets_dir,
     csv_dir,
     default_vals,
+    dependency,
+    min_batch=0,
+    min_rank=0,
     max_batch=0,
     max_rank=0,
-    num_vals=10,
-    up_bound=10,
-    first_dependency="Rank",
-    second_dependency="Batch_size",
-    third_dependency=None,
+    num_vals_batch_size=10,
+    num_vals_rank=10,
 ):
-    if third_dependency:
+    if len(dependency) == 3:
         csv_file_name = (
-            f"try_{first_dependency}_{second_dependency}_{third_dependency}.csv"
+            f"try_{dependency[0]}_{dependency[1]}_{dependency[2]}.csv"
         )
     else:
-        csv_file_name = f"{first_dependency}_{second_dependency}.csv"
+        csv_file_name = f"{dependency[0]}_{dependency[1]}.csv"
     datafiles = [f for f in listdir(datasets_dir) if isfile(join(datasets_dir, f))]
     df = pd.DataFrame(
         columns=[
@@ -74,11 +74,15 @@ def build_csv(
     val_list_dict = default_vals
 
     for i, datafile in enumerate(datafiles):
+        dataset_name = ''
         if datafile[-2:] == "gz":
-            print(datafile[:-10])
+            dataset_name = datafile[:-10]
         elif datafile[-3:] == "zip":
-            print(datafile[:-4])
-        if datafile[:-4] == "Movielens_1m":
+            dataset_name = datafile[:-10]
+
+        print(dataset_name)
+
+        if dataset_name == "Movielens_1m":
             cur_df = get_movielens_data(join(datasets_dir, datafile))
             matr_from_observ, u_id, i_id = matrix_from_observations(
                 cur_df, dtype=float, itemid="movieid"
@@ -89,48 +93,35 @@ def build_csv(
         abs_max_batch = max(matr_from_observ.shape)
         abs_max_rank = min(matr_from_observ.shape)
 
-        val_list_dict[first_dependency] = make_list_params(
-            first_dependency,
-            matr_from_observ,
-            num_vals,
-            up_bound,
-            max_rank=max_rank,
-            max_batch=max_batch,
-        )
-        val_list_dict[second_dependency] = make_list_params(
-            second_dependency,
-            matr_from_observ,
-            num_vals,
-            up_bound,
-            max_rank=max_rank,
-            max_batch=max_batch,
-        )
-        if third_dependency:
-            val_list_dict[third_dependency] = make_list_params(
-                third_dependency,
+        for param in dependency:
+            val_list_dict[param] = make_list_params(
+                param,
                 matr_from_observ,
-                num_vals,
-                up_bound,
+                num_vals_batch_size,
+                num_vals_rank,
+                min_rank=min_rank,
+                min_batch=min_batch,
                 max_rank=max_rank,
                 max_batch=max_batch,
             )
+
         for way in val_list_dict["Way"]:
             if way == "old":
-                st_1 = time.time()
+                svd_time_start = time.time()
                 U, S, V = randomized_svd(matr_from_observ, n_components=max_rank)
                 indices = np.flip(np.argsort(S))
                 new_S = [S[i] for i in indices]
-                et_1 = time.time()
-                print("done svd, time: " + str(et_1 - st_1))
+                svd_time_end = time.time()
+                print("done svd, time: " + str(svd_time_start - svd_time_end))
 
                 for k, rank in enumerate(val_list_dict["Rank"]):
-                    st_2 = time.time()
+                    mult_time_start = time.time()
                     item_space = V.T[:, indices[:rank]] @ np.diag(new_S[:rank])
-                    et_2 = time.time()
+                    mult_time_end = time.time()
 
                     for j, b_s in enumerate(val_list_dict["Batch_size"]):
                         for n_try in val_list_dict["N_tries"]:
-                            st_old = time.time()
+                            old_time_start = time.time()
                             deltas_diams = batched_delta_hyp_old(
                                 item_space,
                                 economic=True,
@@ -138,24 +129,23 @@ def build_csv(
                                 n_tries=n_try,
                                 seed=42,
                             )
-                            et_old = time.time()
+                            old_time_end = time.time()
                             add_data(
                                 deltas_diams=deltas_diams,
-                                time=(et_old - st_old) + (et_1 - st_1) + (et_2 - st_2),
-                                k=rank,
-                                mult_Time=(et_2 - st_2),
-                                svd_Time=(et_1 - st_1),
-                                b_s=(b_s * 100) // abs_max_batch,
+                                time=(old_time_end - old_time_start) + (mult_time_end - mult_time_start) + (svd_time_end - svd_time_start),
+                                k=max(rank, abs_max_rank),
+                                mult_Time=(mult_time_end - mult_time_start),
+                                svd_Time=(svd_time_end - svd_time_start),
+                                b_s=b_s,
                                 n_try=n_try,
                                 way=way,
-                                dataset=datafile[:-10],
+                                dataset=dataset_name,
                                 csv_path=path_to_csv,
                             )
                             print("done try " + str(n_try))
                         print(
-                            "done batch_size "
-                            + str((b_s * 100) // abs_max_batch)
-                            + " %"
+                            "done batch_size " +
+                            str(b_s)
                         )
                     print("done rank " + str(rank))
                 print("done " + str(way))
@@ -163,7 +153,7 @@ def build_csv(
                 for k, rank in enumerate(val_list_dict["Rank"]):
                     for j, b_s in enumerate(val_list_dict["Batch_size"]):
                         for n_try in val_list_dict["N_tries"]:
-                            st = time.time()
+                            new_time_start = time.time()
                             deltas_diams = batched_delta_hyp(
                                 matr_from_observ,
                                 economic=True,
@@ -172,31 +162,30 @@ def build_csv(
                                 n_tries=n_try,
                                 seed=42,
                             )
-                            et = time.time()
+                            new_time_end = time.time()
                             add_data(
                                 deltas_diams=deltas_diams,
-                                time=et - st,
+                                time=new_time_end - new_time_start,
                                 svd_Time=0,
                                 mult_Time=0,
-                                k=rank,
-                                b_s=(b_s * 100) // abs_max_batch,
+                                k=max(rank, abs_max_rank),
+                                b_s=max(b_s, abs_max_batch),
                                 n_try=n_try,
                                 way=way,
-                                dataset=datafile[:-10],
+                                dataset=dataset_name,
                                 csv_path=path_to_csv,
                             )
                             print("done try " + str(n_try))
                         print(
                             "done batch_size "
-                            + str((b_s * 100) // abs_max_batch)
-                            + " %"
+                            + str(b_s)
                         )
-                    print("done rank " + str(rank))
+                    print("done rank " + str(max(rank, abs_max_rank)))
                 print("done " + str(way))
 
 
 def main(
-    datasets_dir, csv_dir, path_to_config, max_batch, max_rank, grid_size, up_bound
+    datasets_dir, csv_dir, path_to_config, min_batch, min_rank, max_batch, max_rank, grid_batch_size, grid_rank
 ):
     with open(path_to_config, "r") as stream:
         try:
@@ -210,13 +199,13 @@ def main(
             datasets_dir,
             csv_dir,
             default_values,
+            dependency,
+            min_batch,
+            min_rank,
             max_batch,
             max_rank,
-            num_vals=grid_size,
-            up_bound=up_bound,
-            first_dependency=dependency[0],
-            second_dependency=dependency[1],
-            third_dependency="Way",
+            num_vals_batch_size=grid_batch_size,
+            num_vals_rank=grid_rank
         )
 
 
@@ -227,18 +216,22 @@ if __name__ == "__main__":
     )
     parser.add_argument("csv_dir", type=str, help="Directory where csvs are stored")
     parser.add_argument("config_path", type=str, help="Path ro config file")
+    parser.add_argument("min_batch", type=int, help="Minimum batch size")
+    parser.add_argument("min_rank", type=int, help="Minimum rank")
     parser.add_argument("max_batch", type=int, help="Maximum batch size")
     parser.add_argument("max_rank", type=int, help="Maximum rank")
-    parser.add_argument("grid_size", type=int, help="num points on each axis")
-    parser.add_argument("up_bound", type=int, help="num factual points on each axis")
+    parser.add_argument("grid_batch_size", type=int, help="Num estimations on batch")
+    parser.add_argument("grid_rank", type=int, help="Num estimations on rank")
 
     args = parser.parse_args()
     main(
         args.datasets_dir,
         args.csv_dir,
         args.config_path,
+        args.min_batch,
+        args.min_rank,
         args.max_batch,
         args.max_rank,
-        args.grid_size,
-        args.up_bound,
+        args.grid_batch_size,
+        args.grid_rank
     )
