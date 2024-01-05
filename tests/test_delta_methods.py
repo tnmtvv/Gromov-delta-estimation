97e4dc8ea59a75af242142da3bc3813373e1b42f
TanyaMatveeva <62231627+tnmtvv@users.noreply.github.com>
2024-01-05 21:35:03 +0300

    Update test_delta_methods.py

--- tests/test_delta_methods.py
+++ tests/test_delta_methods.py
@@ -13,7 +13,7 @@ from lib.source.algo.delta import delta_hyp_GPU
 
 def test_CCL_true_delta():
     dist_matrix = generate_dists(500)
-    far_away_pairs = get_far_away_pairs(dist_matrix, dist_matrix.shape[0] * 70)
+    far_away_pairs = get_far_away_pairs(dist_matrix, dist_matrix.shape[0] * 80)
 
     delta_CCL = delta_hyp_condensed_CCL(typed.List(far_away_pairs), dist_matrix)
     true_delta = delta_hyp(dist_matrix)
